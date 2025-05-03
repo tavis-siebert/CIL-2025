ca8b1b98feda59d0765e678f19e0ffe23baff8d8
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2025-05-03 23:57:48 +0200

    improved __init__.py

--- src/pipelines/__init__.py
+++ src/pipelines/__init__.py
@@ -3,5 +3,5 @@ import importlib
 
 def load_pipeline(pipeline_config):
     module_name, class_name = pipeline_config.name.rsplit(".", maxsplit=1)
-    pipeline_cls = getattr(importlib.import_module(f"pipelines.{module_name}"), class_name)
+    pipeline_cls = getattr(importlib.import_module(f".{module_name}", package=__package__), class_name)
     return pipeline_cls(pipeline_config)
