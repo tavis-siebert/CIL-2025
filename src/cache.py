a7a7936e8c5fe34d2b75e7c44b2729f85dced3c9
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2025-05-10 11:42:40 +0200

    improved typing

--- src/cache.py
+++ src/cache.py
@@ -1,10 +1,13 @@
 import logging
 import pickle
 from pathlib import Path
+from typing import Any, Callable, TypeVar
 
 import numpy as np
 import pandas as pd
 import torch
+
+T = TypeVar("T")
 
 logger = logging.getLogger(__name__)
 
@@ -12,14 +15,34 @@ class Cache:
     def __init__(self, **kwargs):
         self.init(**kwargs)
 
-    def init(self, cache_dir=None):
+    def init(self, cache_dir: str | None = None):
         if cache_dir is not None:
             self.cache_dir = Path(cache_dir)
             logger.info(f"Cache directory: {self.cache_dir}")
         else:
             self.cache_dir = None
 
-    def __call__(self, f, path, refresh=False, save_kwargs={}, load_kwargs={}, verbose=True):
+    def __call__(self, f: Callable[[], T], path: str | Path, refresh: bool = False, save_kwargs: dict[str, Any] = {}, load_kwargs: dict[str, Any] = {}, verbose: bool = True) -> T:
+        """Cache the output of a function to a file.
+
+        Args:
+            f (Callable[[], T]): Function without parameters wrapping around the
+                function call to evaluate and cache.
+            path (str | Path): Path to the file to save the output to. The file
+                extension determines how the output is saved. Supported formats
+                are .npz, .npy, .pt, .csv, and .pkl.
+            refresh (bool, optional): Flag whether the cache should be refreshed
+                or not. Defaults to False.
+            save_kwargs (dict[str, Any], optional): Keyword arguments passed to
+                the saving function. Defaults to {}.
+            load_kwargs (dict[str, Any], optional): Keyword arguments passed to
+                the loading function. Defaults to {}.
+            verbose (bool, optional): Flag whether to be verbose or not.
+                Defaults to True.
+
+        Returns:
+            output (T): Output of the function call.
+        """
         if self.cache_dir is None:
             raise ValueError("Cache directory is not set. Please call init() first.")
         path = Path(self.cache_dir) / path
@@ -30,6 +53,8 @@ class Cache:
             # save to cache
             path.parent.mkdir(parents=True, exist_ok=True)
             if path.suffix == ".npy":
+                if not isinstance(output, np.ndarray):
+                    raise ValueError(f"Output is not a np.ndarray: {type(output)}")
                 np.save(path, output, **save_kwargs)
             elif path.suffix == ".npz":
                 np.savez_compressed(path, **output, **save_kwargs)
@@ -37,11 +62,11 @@ class Cache:
                 torch.save(output, path, **save_kwargs)
             elif path.suffix == ".csv":
                 if not isinstance(output, pd.DataFrame):
-                    raise ValueError(f"Output is not a DataFrame: {type(output)}")
+                    raise ValueError(f"Output is not a pd.DataFrame: {type(output)}")
                 output.to_csv(path, index=False, **save_kwargs)
             elif path.suffix == ".pkl":
-                with open(path, "wb") as f:
-                    pickle.dump(output, f, protocol=pickle.HIGHEST_PROTOCOL, **save_kwargs)
+                with open(path, "wb") as file:
+                    pickle.dump(output, file, protocol=pickle.HIGHEST_PROTOCOL, **save_kwargs)
             else:
                 raise ValueError(f"Unknown file format: {path.suffix}")
             if verbose:
@@ -55,15 +80,15 @@ class Cache:
             elif path.suffix == ".csv":
                 output = pd.read_csv(path, **load_kwargs)
             elif path.suffix == ".pkl":
-                with open(path, "rb") as f:
-                    output = pickle.load(f, **load_kwargs)
+                with open(path, "rb") as file:
+                    output = pickle.load(file, **load_kwargs)
             else:
                 raise ValueError(f"Unknown file format: {path.suffix}")
             if verbose:
                 logger.info(f"Loaded from cache: {path}")
-        return output
+        return output # type: ignore
 
-    def get_path(self, path):
+    def get_path(self, path: str | Path) -> Path:
         if self.cache_dir is None:
             raise ValueError("Cache directory is not set. Please call init() first.")
         return self.cache_dir / path
@@ -71,17 +96,18 @@ class Cache:
 CACHE = Cache()
 
 
-def get_embeddings_folder(pipeline_name, model_name):
+def get_embeddings_folder(pipeline_name: str, model_name: str) -> Path:
+    """Get the cache folder for the embeddings."""
     return Path("embeddings") / pipeline_name / model_name.replace("/", "__")
 
 
-def save_embeddings(embeddings, pipeline_name, model_name, file_name="embeddings.npz", **kwargs):
+def save_embeddings(embeddings: Any, pipeline_name: str, model_name: str, file_name: str = "embeddings.npz", **kwargs):
     """Save embeddings from the cache."""
     path = get_embeddings_folder(pipeline_name, model_name) / file_name
-    return CACHE(lambda: embeddings, path, refresh=True, **kwargs)
+    CACHE(lambda: embeddings, path, refresh=True, **kwargs)
 
 
-def load_embeddings(pipeline_name, model_name, file_name="embeddings.npz", **kwargs):
+def load_embeddings(pipeline_name: str, model_name: str, file_name: str = "embeddings.npz", **kwargs) -> Any:
     """Load embeddings from the cache."""
     path = get_embeddings_folder(pipeline_name, model_name) / file_name
     def raise_not_found():
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,18 @@
 import logging
 import os
 import sys
+from pathlib import Path
+from typing import Any
 
 import pandas as pd
 import torch
-from omegaconf import OmegaConf
+from omegaconf import DictConfig, ListConfig, OmegaConf
 from sklearn.metrics import mean_absolute_error
 
 logger = logging.getLogger(__name__)
 
 
-def setup_logging(level=logging.INFO, format=None, dateformat=None, other_loggers=[]):
+def setup_logging(level: int = logging.INFO, format: str | None = None, dateformat: str | None = None):
     if format is None:
         format="[%(asctime)s] %(levelname)-8s %(name)s:%(lineno)-5d %(message)s"
     if dateformat is None:
@@ -26,21 +28,21 @@ def setup_logging(level=logging.INFO, format=None, dateformat=None, other_logger
     logging.captureWarnings(True)
 
 
-def get_config(config_path, overwrite={}, verbose=True):
+def get_config(config_path: str | Path, overwrite: dict[str, Any] = {}, verbose: bool = True) -> DictConfig | ListConfig:
     # load the config file
     config = OmegaConf.load(config_path)
 
     # overwrite the config
     if isinstance(overwrite, dict):
-        overwrite = [f"{key}={value if value is not None else 'null'}" for key, value in overwrite.items()]
-    config.merge_with_dotlist(overwrite)
+        overwrite_dotlist = [f"{key}={value if value is not None else 'null'}" for key, value in overwrite.items()]
+    config.merge_with_dotlist(overwrite_dotlist)
 
     if verbose:
         logger.info(f"Loaded config: {config_path}")
     return config
 
 
-def get_device(device="auto", verbose=True):
+def get_device(device: str | torch.device = "auto", verbose: bool = True) -> torch.device:
     # create the device handler
     if device == "auto":
         if torch.cuda.is_available():
@@ -62,23 +64,23 @@ def _check_valid_labels(labels):
         raise ValueError(f"Labels contain invalid labels. Allowed labels are: {valid_labels}")
 
 
-def load_data(path):
+def load_data(path: str | Path) -> pd.DataFrame:
     dataset = pd.read_csv(path, index_col=0)
     return dataset
 
 
-def apply_label_mapping(labels, label_mapping):
+def apply_label_mapping(labels, label_mapping: dict[str, Any]) -> pd.Series:
     labels = pd.Series(labels).map(label_mapping)
     return labels
 
 
-def apply_inverse_label_mapping(labels, label_mapping):
+def apply_inverse_label_mapping(labels, label_mapping: dict[str, Any]) -> pd.Series:
     label_mapping_rev = {value: key for key, value in label_mapping.items()}
     labels = pd.Series(labels).map(label_mapping_rev)
     return labels
 
 
-def evaluate_score(labels, predictions):
+def evaluate_score(labels, predictions) -> float:
     _check_valid_labels(labels)
     _check_valid_labels(predictions)
 
@@ -89,10 +91,10 @@ def evaluate_score(labels, predictions):
 
     # compute the score
     score = 0.5 * (2 - mean_absolute_error(labels, predictions))
-    return score
+    return float(score)
 
 
-def save_predictions(path, ids, predictions):
+def save_predictions(path: str | Path, ids, predictions):
     _check_valid_labels(predictions)
 
     os.makedirs(os.path.dirname(path), exist_ok=True)
