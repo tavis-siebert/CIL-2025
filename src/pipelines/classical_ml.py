ece35a655b5d4b6b14c08a1e7aa099ddc80f275d
Samuel Stante <samuel.stante@gmail.com>
2025-05-21 15:54:05 +0200

    add glove

--- src/pipelines/classical_ml.py
+++ src/pipelines/classical_ml.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from omegaconf import OmegaConf
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
 from preprocessing import apply_preprocessing
-from utils import apply_inverse_label_mapping, apply_label_mapping
+from utils import (
+    apply_inverse_label_mapping,
+    apply_label_mapping,
+    load_glove_embeddings,
+    sentences_to_glove_embeddings,
+)
 
 from .base import BasePipeline
 
@@ -27,37 +33,48 @@ class ClassicalMLPipeline(BasePipeline):
         config_vectorizer = OmegaConf.to_container(config.vectorizer)
         if "ngram_range" in config_vectorizer:
             config_vectorizer["ngram_range"] = tuple(config_vectorizer["ngram_range"])
-        vectorizer_type = config_vectorizer.pop("type")
-        if vectorizer_type == "CountVectorizer":
+        self.vectorizer_type = config_vectorizer.pop("type")
+        if self.vectorizer_type == "CountVectorizer":
             self.vectorizer = CountVectorizer(**config_vectorizer)
-        elif vectorizer_type == "TfidfVectorizer":
+        elif self.vectorizer_type == "TfidfVectorizer":
             self.vectorizer = TfidfVectorizer(**config_vectorizer)
+        elif self.vectorizer_type == "GloVe":
+            glove_path = config_vectorizer.pop("path")
+            self.glove_embeddings = load_glove_embeddings(glove_path)
         else:
             raise ValueError(f"Unknown vectorizer type: {config_vectorizer['type']}")
 
         # configure model
         config_model = OmegaConf.to_container(config.model)
-        model_type = config_model.pop("type")
-        if model_type == "LogisticRegression":
+        self.model_type = config_model.pop("type")
+        if self.model_type == "LogisticRegression":
             self.model = LogisticRegression(**config_model)
+        elif self.model_type == "RandomForestClassifier":
+            self.model = RandomForestClassifier(**config_model)
+        elif self.model_type == "GradientBoostingClassifier":
+            self.model = GradientBoostingClassifier(**config_model)
         else:
             raise ValueError(f"Unknown model type: {config_model['type']}")
 
         # configure preprocessing
-        self.preprocessing_rules = set(OmegaConf.to_container(config.preprocessing)) if config.preprocessing else None
+        self.preprocessing_rules = set(OmegaConf.to_container(config.preprocessing)) if "preprosessing" in config else None
 
     def train(self, train_sentences, train_labels, val_sentences, val_labels):
         # apply label mapping
         train_labels = apply_label_mapping(train_labels, self.label_mapping)
         val_labels = apply_label_mapping(val_labels, self.label_mapping)
 
-        # apply preprocessing
+        # apply preprocessing for train
         if self.preprocessing_rules:
             train_sentences = apply_preprocessing(train_sentences, self.preprocessing_rules)
-            val_sentences = apply_preprocessing(val_sentences, self.preprocessing_rules)
+
+        # get embeddings for train
+        if self.vectorizer_type == "GloVe":
+            train_embeddings = sentences_to_glove_embeddings(train_sentences, self.glove_embeddings)
+        else:
+            train_embeddings = self.vectorizer.fit_transform(train_sentences)
 
         # train
-        train_embeddings = self.vectorizer.fit_transform(train_sentences)
         self.model.fit(train_embeddings, train_labels)
 
         # predict
@@ -71,8 +88,13 @@ class ClassicalMLPipeline(BasePipeline):
         if self.preprocessing_rules:
             sentences = apply_preprocessing(sentences, self.preprocessing_rules)
 
-        # predict on sentences
-        embeddings = self.vectorizer.transform(sentences)
+        # get embeddings
+        if self.vectorizer_type == "GloVe":
+            embeddings = sentences_to_glove_embeddings(sentences, self.glove_embeddings)
+        else:
+            embeddings = self.vectorizer.transform(sentences)
+
+        # predict
         predictions = self.model.predict(embeddings)
 
         # apply inverse label mapping
